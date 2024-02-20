ac9e74998796b3ffe94c00e9773f581847a35fa9
hhhh <abourkiachaab@gmail.com>
2024-02-20 09:49:49 +0100

    black_jack

---
+++ src/BlackJack.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BlackJack {
+    public static La_main laMain = new La_main();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        System.out.println("  BIENVENUE AU BLACKJACK   ");
+        System.out.println("  1. Commencer             ");
+        System.out.println("  2. Exit                  ");
+        System.out.print("Enter your choice: ");
+        choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                laMain.Distribuer_cartes();
+                break;
+            case 2 :
+
+                break;
+            default:
+                System.out.println("Entrer 1 ou 2");
+        }
+    }
+}
---
+++ src/La_main.java
@@ -0,0 +1,66 @@
+
+import java.util.Scanner;
+
+public class La_main {
+
+        public void Distribuer_cartes(){
+            PaquetCartes paquet1 = new PaquetCartes();
+            // Tirer plusieurs cartes et afficher les résultats
+            System.out.println("================les cartes de croupier :================");
+            for (int i = 0; i < 2; i++) {
+                String card = paquet1.melanger();
+                System.out.println("Carte tirée : " + card);
+                System.out.println();
+            }
+            System.out.println("Total Value: " + paquet1.getTotalValuePlayer());
+            PaquetCartes paquet2 = new PaquetCartes();
+            System.out.println("================Vos cartes :================");
+            for (int i = 0; i < 2; i++) {
+                String card = paquet2.melanger();
+                System.out.println("Carte tirée : " + card);
+                System.out.println();
+            }
+            System.out.println("Total Value: " + paquet2.getTotalValuePlayer());
+
+            if (paquet2.getTotalValuePlayer() < 21){
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("================1 pour STAND================");
+                System.out.println("================2 pour HIT================");
+                int choix;
+                choix = scanner.nextInt();
+                switch (choix) {
+                    case 1 :
+
+                        break;
+                    case 2 :
+                        String card = paquet2.melanger();
+                        System.out.println("Carte tirée : " + card);
+                        System.out.println();
+                        System.out.println("Total Value: " + paquet2.getTotalValuePlayer());
+                        break;
+                    default:
+                        System.out.println("Choisit 1 ou 2 !!");
+                }
+            }
+            if (paquet2.getTotalValuePlayer() > 21){
+                System.out.println("YOU LOST!");
+            }
+            else
+            {
+                int choix;
+                Scanner scan = new Scanner(System.in);
+                while (paquet2.getTotalValuePlayer() < 21) {
+                    System.out.println("================1 pour STAND================");
+                    System.out.println("================2 pour HIT================");
+                    choix = scan.nextInt();
+                    String card = paquet2.melanger();
+                    System.out.println("Carte tirée : " + card);
+                    System.out.println();
+                    System.out.println("Total Value: " + paquet2.getTotalValuePlayer());
+                }
+                if (paquet2.getTotalValuePlayer() > 21){
+                    System.out.println("YOU LOST!");
+                }
+            }
+        }
+    }
